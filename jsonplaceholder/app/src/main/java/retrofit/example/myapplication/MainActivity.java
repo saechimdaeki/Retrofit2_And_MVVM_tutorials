86b5ddd246d75ad4af2d5ec43bcc4d26032988e5
김준성 <anima94@naver.com>
2020-05-17 21:50:34 +0900

    Post Request & FormUrlEncoded :star2:

--- jsonplaceholder/app/src/main/java/retrofit/example/myapplication/MainActivity.java
+++ jsonplaceholder/app/src/main/java/retrofit/example/myapplication/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
          jsonPlaceHoderApi=retrofit.create(JsonPlaceHoderApi.class);
 
        // getPosts();
-        getComments();
+      //  getComments();
+        createPost();
     }
     private void getPosts(){
         Map<String,String> parameters=new HashMap<>();
@@ -89,6 +90,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<List<Comment>> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+    private void createPost(){
+        Post post=new Post(23,"New Title","New Text");
+       Map<String,String> fields=new HashMap<>();
+       fields.put("userId","25");
+       fields.put("title","New Title");
+        Call<Post> call=jsonPlaceHoderApi.createPost(fields);
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                        if(!response.isSuccessful()){
+                            textViewResult.setText("Code: "+response.code());
+                            return;
+                        }
+                        Post postResponse=response.body();
+                String content="";
+                content+="Code:"+response.code()+"\n";
+                content+="ID: "+postResponse.getId()+"\n";
+                content+="User Id: "+postResponse.getUserId()+"\n";
+                content+="Title: "+postResponse.getTitle()+"\n";
+                content+="Text: "+postResponse.getText()+"\n";
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
             }
         });
     }
