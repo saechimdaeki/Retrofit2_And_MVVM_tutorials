fa3e567b9ff55904befaf0ae703ef8dc88449a1c
김준성 <anima94@naver.com>
2020-07-02 00:14:08 +0900

    MVVM SETTING :city_sunset:

---
+++ MVVM_tutorial/kotlin_MVVM/app/src/main/java/junseong/kotlin/kotlin_mvvm/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package junseong.kotlin.kotlin_mvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import junseong.kotlin.kotlin_mvvm.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
